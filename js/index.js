020a1b60813b120b922dc53c0fefce2e8cf6a1da
Zuhriddinziko <169124288+Zuhriddinziko@users.noreply.github.com>
2024-06-17 13:29:21 +0500

    dars

---
+++ js/index.js
@@ -0,0 +1,329 @@
+// masala 1
+// function colors(color1, color2, color3) {
+//     let res = [];
+//     res.push(color1);
+//     res.push(color2);
+//     res.push(color3);
+
+//     return res;
+// }
+// console.log(colors('yashil', 'oq', 'qora'))
+
+
+// masala 2
+// function colors(lor1, lor2, lor3) {
+//     let res = [];
+//     res.push(lor1);
+//     res.push(lor2);
+//     res.push(lor3);
+
+//     return res;
+// }
+// console.log(colors(1, 5, 5))
+
+
+// masala 3
+// function colors(lor1, lor2, lor3) {
+//     let res = [];
+
+//     res.push(lor1);
+//     res.push(lor2);
+//     res.push(lor3);
+
+
+//     return res;
+// }
+// console.log(colors(1,2,3)[0])
+// console.log(colors(1,2,3)[2])
+
+
+// masala 4
+// function colors(lor1, lor2, lor3, lor4) {
+//     let res = [];
+
+//     res.push(lor1);
+//     res.push(lor2);
+//     res.push(lor3);
+//     res.push(lor4);
+//     res.splice(1,1,5) 
+
+//     return res;
+// }
+// console.log(colors(1,2,3,6));
+
+// // masala 5
+//  function numberss(){
+//     const numbers = [1, 2, 3, 4];
+//     numbers.push(80);
+//     return numbers;
+
+//  }
+// console.log (numberss());
+
+// masala 6
+
+// function numberss(){
+//         const numbers = [1, 2, 3, 4];
+//         numbers.pop();
+//         return numbers;
+
+//      }
+//     console.log (numberss());
+
+
+
+// masala 7
+
+
+
+
+// let movi = ['olma', 'anor', 'alcha',"behi", 4, 5];
+// console.log(movi.length)
+
+
+
+
+// masala 8
+
+// let numbers = [1,2,3,4,5,6,7];
+// numbers.unshift(23)
+// console.log(numbers)
+
+
+// masala 9
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+// numbers.shift()
+// console.log(numbers)
+
+// masala 10
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+// let futres = [8, 89, 56];
+// let sum = numbers.concat(futres);
+// console.log(sum);
+
+// masala 11
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+// console.log(numbers.includes(6))
+
+// masala 12
+// let future = ['olma', 'anor', 'gilos', 'orik', 'shoftoli', 'behi', 'nok'];
+// console.log(future.sort())
+
+// masala 13
+// let future = ['olma', 'anor', 'gilos', 'orik', 'shoftoli', 'behi', 'nok'];
+// console.log(future.indexOf('gilos'))
+
+
+// masala 14
+// let future = ['olma', 'anor', 'gilos', 'orik', 'shoftoli', 'behi', 'nok'];
+// console.log(future[1])
+
+
+// masala 15
+// let future = ['olma', 'anor', 'gilos', 'orik', 'shoftoli', 'behi', 'nok'];
+// future.pop();
+// future.pop();
+// console.log(future)
+
+// masala 16
+// let future = ['olma', 'anor', 'gilos', 'orik', 'shoftoli', 'behi', 'nok'];
+// future.push('anjir');
+// future.push('banan');
+// console.log(future)
+
+// masala 17
+// let future = ['olma', 'anor', 'gilos', 'orik', 'shoftoli', 'behi', 'nok'];
+// future[0]= ('qulpinay');
+// future[future.length-1] = ('apelsin');
+// console.log(future)
+
+
+// masala 18
+// let future = ['olma', 'anor', 'gilos', 'orik', 'shoftoli', 'behi', 'nok'];
+// future.length = 0;
+// console.log(future);
+
+
+// masala 19
+// let future = ['olma', 'anor', 'gilos', 'orik', 'shoftoli', 'behi', 'nok'];
+// let copyFuture = [...future];
+
+// console.log(copyFuture);
+
+
+// masala 20
+// let future = [1, 2, 3, 4,5, 6, 7];
+// let newarr = [];
+//  for (let i = 1; i<future.length; i++){
+//     newarr.push(i);
+//     newarr.push(i);
+//  }
+
+// console.log(newarr);
+
+
+// masala 21
+// let future = [1, 2, 35, 46, 50, 66, 7];
+// let newarr = Math.max(...future);
+// console.log(newarr)
+
+
+// let future = [1, 2, 35, 46, 50, 66, 7];
+// let min ="";
+// for (let i=0; i<future.length; i++){
+//     if(future[i]>min){
+//         min=future[i]
+//     }else{
+//         min=min
+//     }
+    
+// }
+
+// console.log(min)
+
+// masala 22
+// let future = [1, 2, 35, 46, 50, 66, 7];
+// let newarr = Math.min(...future);
+// console.log(newarr)
+
+
+
+// masala 23
+// let future = [1, 2, 35];
+// let min ="";
+// for (let i=0; i<future.length; i++){
+//     if(future[i]>min){
+//         min=future[i]
+//     }else{
+//         min=min
+//     }
+    
+// }
+
+// console.log(min)
+
+// let future = [5, 2, 35];
+// let min =0;
+// for (let i=0; i<future.length; i++){
+//     if(future[i]<min){
+//         min = future[i]
+//     }else if(min<future[i]){
+//         min=min
+//     } else(
+//         min = future[i]
+//     )
+    
+// }
+
+// console.log(min)
+
+
+
+
+
+
+
+
+
+
+
+
+// Massiv metodlariga oid masalalar.
+// Quyidagi masalarni funksiyalar yordamida ishlang.
+
+// masala 1
+// let arrayPush = ['salom', 10, 45, 56, 'salom']
+// arrayPush.push('damas')
+// console.log(arrayPush)
+
+// masala 2
+// let arrayPush = ['salom', 10, 45, 56, 'salom']
+// arrayPush.pop()
+// console.log(arrayPush)
+
+// masala 3
+// let arrayPush = ['salom', 10, 45, 56, 'salom']
+// arrayPush.shift()
+// console.log(arrayPush)
+
+// masala 4
+// let arrayPush = ['salom', 10, 45, 56, 'salom']
+// arrayPush.unshift(456)
+// console.log(arrayPush)
+
+// masala 5
+// let arrayPush = ['salom', 10, 45, 56, 'salom']
+// console.log(arrayPush.includes(10))
+
+// // masala 6
+// let arrayPush = ['salom', 10, 45, 56, 'salom']
+// console.log(arrayPush.indexOf("salom"))
+
+// masala 7
+// let arrayPush = ['salom', 10, 45, 56, 'salom']
+// console.log(arrayPush.lastIndexOf("salom"))
+
+// masala 8
+// function exsplise (arr, element){
+//     let res = arr.splice(element)
+//     return res;
+// }
+// console.log(exsplise([1,2,3],1,1))
+
+// // masala 9
+// function exsplise (arr, element){
+//     let res = arr.push(element)
+//     return res;
+// }
+// console.log(exsplise([1,2,3],1,1))
+
+
+
+// masala 10
+// function exsplise (arr, element){
+//     let res = arr.pop(element)
+//     return res;
+// }
+// console.log(exsplise([1,2,3]))
+
+// masala 11
+// function exsplise (arr, element){
+//     let res = arr.shift(element)
+//     return res;
+// }
+// console.log(exsplise([1,2,3],1))
+
+// masala12
+// function exsplise (arr, element){
+//     let res = arr.unshift(element)
+//     return res;
+// }
+// console.log(exsplise([1,2,3],1))
+
+// masala 13
+// function exsplise (arr, element){
+//         let res = arr.includes(element)
+//         return res;
+//     }
+//     console.log(exsplise([1,2,3],1))
+
+// // masala 14
+// function exsplise (arr, element){
+//     let res = arr.indexOf(element)
+//     return res;
+// }
+// console.log(exsplise([1,2,3],1))
+
+// masala 15
+// function exsplise (arr, element){
+//     let res = arr.lastIndexOf(element)
+//     return res;
+// }
+// console.log(exsplise([1,2,3],2))
+
+// masala 16
+// function exsplise (arr, element){
+//         let res = arr.splice(element)
+//         return res;
+//     }
+//     console.log(exsplise([1,2,3],2,1))
